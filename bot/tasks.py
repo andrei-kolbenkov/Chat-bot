c12d5f281096cb9b65d2f7529d125a3de6ce3b14
Andrei Kolbenkov <andrei.kolbenkov@gmail.com>
2025-03-20 04:39:19 +0300

    Added logging

--- bot/tasks.py
+++ bot/tasks.py
@@ -1,6 +1,7 @@
 from celery import Celery
 from aiogram import Bot
 import asyncio
+import logging
 import os
 from dotenv import load_dotenv
 
@@ -11,14 +12,22 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 # Инициализация бота
 bot = Bot(token=BOT_TOKEN)
 
+
+logger = logging.getLogger(__name__)
+
 # Создаем Celery приложение
 celery = Celery('tasks', broker='redis://redis:6379/0')
 
 # Асинхронная функция для отправки сообщения
 async def delayed_send(chat_id, text):
-    # С помощью Celery можно разные тяжелые задачи которые занимают время выполнять в фоне
-    await asyncio.sleep(10) # Ждем 10 секунд
-    await bot.send_message(chat_id, text) # Отправляем текст обратно пользователю
+    try:
+        # С помощью Celery можно разные тяжелые задачи которые занимают время выполнять в фоне
+        await asyncio.sleep(10)  # Ждем 10 секунд
+        await bot.send_message(chat_id, text)  # Отправляем текст обратно пользователю
+        logger.info(f"Сообщение {text} отправлено пользователю {chat_id}")
+    except Exception as e:
+        logger.error(f"Ошибка при отправке сообщения в chat_id={chat_id}: {e}")
+        raise
 
 # Celery задача
 @celery.task
